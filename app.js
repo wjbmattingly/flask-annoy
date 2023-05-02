925c150bd1e27c82de880b43c40a9ab8a9a55298
William Mattingly <62964060+wjbmattingly@users.noreply.github.com>
2023-05-02 20:14:10 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const queryForm = document.getElementById("query-form");
+
+    // Initialize the DataTable
+    let dataTable = $('#results-table').DataTable({
+        columns: [], // Initialize with empty columns
+        order: [], // Disable initial sorting
+    });
+
+    queryForm.addEventListener("submit", async (e) => {
+        e.preventDefault();
+        const inputQuery = document.getElementById("input_query").value;
+        const numResults = document.getElementById("num_results").value;
+
+        try {
+            const response = await fetch(`https://streamlit.as.uky.edu/flask-annoy/query?input_query=${encodeURIComponent(inputQuery)}&num_results=${encodeURIComponent(numResults)}`);
+            const data = await response.json();
+
+            if (data.error) {
+                dataTable.clear().draw();
+            } else if (Array.isArray(data.similar_items)) {
+                // Get the keys from the first item to use as column headers
+                const columns = Object.keys(data.similar_items[0]).map(key => ({ title: key, data: key }));
+
+                // Update the columns and data
+                dataTable.clear();
+                dataTable.destroy();
+                dataTable = $('#results-table').DataTable({
+                    data: data.similar_items,
+                    columns: columns,
+                    order: [],
+                    autoWidth: false // Add this line
+                });
+
+                // Adjust column sizes after updating the data
+                dataTable.columns.adjust().draw();
+            } else {
+                dataTable.clear().draw();
+            }
+        } catch (error) {
+            dataTable.clear().draw();
+        }
+    });
+});
